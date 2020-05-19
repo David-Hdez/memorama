d8bf888b328964e7d46a2f8afaa6350bb03faf88
David Alfredo Pacheco Hernandez <david.alfredo.hdez@outlook.com>
2020-05-18 20:30:25 -0500

    Tarjetas y reiniciar
    No contar la misma tarjeta dos veces

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {    
+document.addEventListener('DOMContentLoaded', () => {
     const tarjetasJuego = [
         {
             nombre: 'carro',
@@ -50,60 +50,73 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
-    tarjetasJuego.sort(()=>0.5-Math.random());//Obtener las tarjetas aleatoriamente
+    tarjetasJuego.sort(() => 0.5 - Math.random());//Obtener las tarjetas aleatoriamente
 
-    const puntos=document.querySelector('#puntuaje')
-    const tablero=document.querySelector('.tablero');
-    var tarjetasElegidas=[];
-    var tarjetasElegidasID=[];
-    var tarjetasGanadas=[];
+    const puntos = document.querySelector('#puntuaje')
+    const tablero = document.querySelector('.tablero');
+    var tarjetasElegidas = [];
+    var tarjetasElegidasID = [];
+    var tarjetasGanadas = [];
 
     //Creando tablero
-    function crearTablero(){        
-        for (let index = 0; index < tarjetasJuego.length; index++) {         
-            var tarjeta=document.createElement('img');
-            tarjeta.setAttribute('src','imagenes/oculto.png');
-            tarjeta.setAttribute('data-id',index);
-            tarjeta.addEventListener('click',voltearTarjeta);
+    function crearTablero() {
+        for (let index = 0; index < tarjetasJuego.length; index++) {
+            var tarjeta = document.createElement('img');
+            tarjeta.setAttribute('src', 'imagenes/oculto.png');
+            tarjeta.setAttribute('data-id', index);
+            tarjeta.addEventListener('click', voltearTarjeta);
             tablero.appendChild(tarjeta);
         }
     }
 
     //Coinciden las tarjetas 
-    function checkCoincide(){
-        var tarjetas=document.querySelectorAll('img');
-        const opcionUnoId=tarjetasElegidasID[0];
-        const opcionDosId=tarjetasElegidasID[1];           
-        
-        if (tarjetasElegidas[0]==tarjetasElegidas[1]) {                
+    function checkCoincide() {
+        var tarjetas = document.querySelectorAll('img');
+        const opcionUnoId = tarjetasElegidasID[0];
+        const opcionDosId = tarjetasElegidasID[1];
+
+        if (tarjetasElegidas[0] == tarjetasElegidas[1]) {
             swal('¡Haz encontrado su coincidencia!');
             /*tarjetas[opcionUnoId].setAttribute('src','imagenes/blanco.jpg');
             tarjetas[opcionDosId].setAttribute('src','imagenes/blanco.jpg');*/
             tarjetasGanadas.push(tarjetasElegidas);
-        }else{
-            tarjetas[opcionUnoId].setAttribute('src','imagenes/oculto.png');
-            tarjetas[opcionDosId].setAttribute('src','imagenes/oculto.png');            
+        } else {
+            tarjetas[opcionUnoId].setAttribute('src', 'imagenes/oculto.png');
+            tarjetas[opcionDosId].setAttribute('src', 'imagenes/oculto.png');
+            tarjetas[opcionUnoId].addEventListener('click', voltearTarjeta);
+            tarjetas[opcionDosId].addEventListener('click', voltearTarjeta);
         }
 
-        tarjetasElegidas=[];
-        tarjetasElegidasID=[];
-        puntos.textContent=tarjetasGanadas.length;
+        tarjetasElegidas = [];
+        tarjetasElegidasID = [];
+        puntos.textContent = tarjetasGanadas.length;
 
-        if (tarjetasGanadas==tarjetasJuego/2) {
-            puntos.textContent='¡Haz encontrado todas las tarjetas!';
+        if (tarjetasGanadas.length == tarjetasJuego.length / 2) {
+            puntos.textContent = '¡Haz encontrado todas las tarjetas!';
+
+            swal({
+                title: "¿Volver a jugar?",
+                icon: "success"
+            })
+                .then((restart) => {
+                    if (restart) {
+                        location.reload();
+                    }
+                });
         }
     }
 
     //Voltear tarjetas
-    function voltearTarjeta(){
-        var idTarjeta=this.getAttribute('data-id');        
+    function voltearTarjeta() {
+        var idTarjeta = this.getAttribute('data-id');
+        this.removeEventListener('click', voltearTarjeta);//No volver a elegir la tarjeta
         tarjetasElegidas.push(tarjetasJuego[idTarjeta].nombre);
-        tarjetasElegidasID.push(idTarjeta);             
-        this.setAttribute('src',tarjetasJuego[idTarjeta].img); 
+        tarjetasElegidasID.push(idTarjeta);
+        this.setAttribute('src', tarjetasJuego[idTarjeta].img);
 
         //Verifica si coinciden las tarjetas
-        if (tarjetasElegidas.length==2) {
-            setTimeout(checkCoincide,100);                        
+        if (tarjetasElegidas.length == 2) {
+            setTimeout(checkCoincide, 100);
         }
     }
 
